c3dd128f7939ab83e03a91968ea8f913b34f76d6
Batuhan Akkaya <mail@batuhanakkaya.com>
2019-07-10 14:01:15 +0300

    header comp added

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <ul>
+                    <li>Home Page</li>
+                    <li>Todo Page</li>
+                    <li>Users Page</li>
+                </ul>
+            </header>
+        );
+    }
+}
+
+export default Header;
